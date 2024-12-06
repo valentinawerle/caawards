9a71fb2dda96b2433dae39967d02b4ca60f68901
_ignacio.dev_ <protec83007@gmail.com>
2024-12-06 02:25:14 -0300

    About page complete

---
+++ src/data/about.ts
@@ -0,0 +1,9 @@
+export const aboutParagraphs = [
+    'Los Coscu Army Awards es un evento que premia y homenajea a streamers de la comunidad de Twitch de Argentina, dirigido por el streamer argentino Martín Pérez Disalvo.',
+    'En 2018, organizó el primer evento de la Coscu Army Awards en la Escuela Da Vinci, y fue transmitido por Twitch. El evento alcanzó un pico de 40 mil espectadores, superando al streamer más popular de la plataforma en el mundo en esa época.',
+    'En 2019, se realizó la segunda edición en el Estadio Obras Sanitarias de CABA, contó con un pico de casi 100 mil espectadores.',
+    'En 2020, se organizó la tercera edición en el Estadio Obras Sanitarias, superó los 425 mil espectadores y se convirtió en la transmisión más vista de la plataforma en Latinoamérica, y los streamers le otorgaron a Coscu el reconocimiento de "Streamer Leyenda".',
+    'En 2021, se realizó la cuarta edición en el Hotel Hilton. Además homenajearon a Frankkaster, Grafo y Pimpeano ganadores en años anteriores con un anillo de oro como obsequio.',
+    'En 2022, hubo un salto importante para la comunidad al realizar la quinta edición en el monumento histórico de Argentina: el Teatro Colón, contó con más de 200 mil espectadores y más de 2.500 invitados.',
+    'En 2023, Tuvimos la oportunidad de tener la sexta edición en el Luna Park, teniendo casi una duración de 6 horas, el streamer del año fue DavoXeneize, quien dedicó el premio a Juan Román Riquelme.',
+];

c12c403ad913f0748be5a98460c5fae8d0dce57f
_ignacio.dev_ <protec83007@gmail.com>
2024-12-07 21:08:11 -0300

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless"
 
 // @ts-ignore
 import tailwind from "@astrojs/tailwind";
@@ -11,6 +12,11 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
   output: "server",
   i18n: {
     defaultLocale: "es",
